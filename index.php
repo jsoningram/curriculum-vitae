e528b8922296a266d4879bdd1a10cc450fc1ddd1
jsoningram@gmail.com <jasoningram@Jasons-MacBook-Pro.local>
2015-07-29 10:46:24 -0700

    wording change, add to index

--- index.php
+++ index.php
@@ -75,7 +75,7 @@
 					<?php
 						foreach ($json->sites as $site) :
 							echo '<div class="row"><div class="small-12 medium-2 columns">';
-							//if ($site->disclaimer) : echo '<p><em>' . $site->disclaimer . '</em></p>'; endif;
+							if ($site->disclaimer) : echo '<p><em>' . $site->disclaimer . '</em></p>'; endif;
 							echo '</div><!--/.columns -->';
 							echo '<div class="small-12 medium-10 columns sites">';
 								echo $site->company . '<br /><a class="url" rel="nofollow" href="' . $site->url . '">' . $site->url . '</a><p>' . $site->description . '</p>';
